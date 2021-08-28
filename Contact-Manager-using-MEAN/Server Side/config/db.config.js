c612e0867b19eaa55626932ffb9d11ffac99a8c2
Swati Swagatika Pradhan <66533554+Swati-Swagatika@users.noreply.github.com>
2021-08-28 20:46:48 +0530

    add your password

--- Contact-Manager-using-MEAN/Server Side/config/db.config.js
+++ Contact-Manager-using-MEAN/Server Side/config/db.config.js
@@ -1,4 +1,4 @@
 const config = {
-    uri:"mongodb+srv://swati-swagatika-2020:swati1999@cluster0.dytkh.mongodb.net/courseProject?retryWrites=true&w=majority"
+    uri:"mongodb+srv://swati-swagatika-2020:[Password]@cluster0.dytkh.mongodb.net/courseProject?retryWrites=true&w=majority"
 }
 module.exports = config;
